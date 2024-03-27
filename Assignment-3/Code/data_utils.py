144f7b18c0f73c4c9bcad570a7a64430e27e2b4f
Gowtham Venkatesan <g.venkatesan@student.vu.nl>
2024-03-27 11:50:25 +0100

    fixed bug in conll transform file

--- Assignment-3/Code/data_utils.py
+++ Assignment-3/Code/data_utils.py
@@ -42,28 +42,27 @@ def conll_transform(df):
     """
     Transform the conll df by duplicating sentences with more than one predicate,
     such that each sentence has exactly one predicate.
+    Add a random number to the 'sent_id' column for each duplicated sentence.
     """
-    # regex to match predicate
     regex = '.*\.0\d'
-    # Group by sentence id and filter sentences with more than one predicate
     multi_predicate_sentence_ids = df.groupby('sent_id').filter(
         lambda x: x.iloc[:, 11].str.match(regex).sum() > 1)['sent_id'].unique()
-    # Accumulate duplicate rows to be concatenated in a list
     rows_to_concat = []
     for sent_id in multi_predicate_sentence_ids:
         sentence = df[df['sent_id'] == sent_id]
-        # get count of predicates
         predicate_count = sentence.iloc[:, 11].str.match(regex).sum()
-        # duplicate the sentence for each predicate and copy the 13th column to the 12th column and so on
         for i in range(1, predicate_count):
             sentence_copy = sentence.copy(deep=True)
             sentence_copy.iloc[:, 12] = sentence_copy.iloc[:, 12+i]
+            # Add a random number to the 'sent_id' column
+            sentence_copy['sent_id'] = sentence_copy['sent_id'] + '_' + str(i) # add the duplication number for easier grouping
+            #print(sentence_copy.columns)
+            sentence_copy.loc[sentence_copy[12] != 'V', 11] = '_' # set the predicate to '_' for non-predicate words for the subsequent predicates after the first
             rows_to_concat.append(sentence_copy)
-    # Concatenate the original and duplicated sentences
     df = pd.concat([df] + rows_to_concat, ignore_index=True)
-    # drop columns starting from 13th
     df = df.drop(df.columns[13:], axis=1)
     df = df.rename(columns={11: 'predicate', 12: 'argument_type'})
+    df.loc[df['argument_type'] != 'V', 'predicate'] = '_' # set the argument type to '_' for non-predicate words for the first predicate
     df = df.fillna('_')
     return df
 
